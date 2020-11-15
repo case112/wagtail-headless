e6c14081652478bf6dc0464b1cdae7253a19cdcf
Oskar <case112@users.noreply.github.com>
2020-11-15 12:33:44 +0200

    Routing and fetching the data from the same file, added styling and an image field

--- backend/news/models.py
+++ backend/news/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 from wagtail.api import APIField
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.images.api.fields import ImageRenditionField
 
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
@@ -10,15 +12,23 @@ class NewsPage(Page):
     date = models.DateField("Post date")
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
 
     content_panels = Page.content_panels + [
         FieldPanel('date'),
         FieldPanel('intro'),
         FieldPanel('body', classname="full"),
+        ImageChooserPanel('image'),
     ]
 
     api_fields = [
         APIField('date'),
         APIField('intro'),
-        APIField('body')
+        APIField('body'),
+        APIField('image_thumbnail', serializer=ImageRenditionField('fill-300x300', source='image')),
 ]
