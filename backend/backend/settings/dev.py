6a1451875a53e600074bfbf6ccaa594fb5c94c5c
Oskar <case112@users.noreply.github.com>
2020-11-15 10:10:53 +0200

    Set up backend, django and wagtail

---
+++ backend/backend/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '$c_0h@so!qsl-wnrl4^x8*0l**3*pqnq55i3+=odq)wq(c7-@t'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
